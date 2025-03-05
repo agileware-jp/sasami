e340038c81bb052c76ffb9119171969856d86ca2
kondai24 <DaisukeKondo1215@outlook.jp>
2025-03-05 16:34:24 +0900

    formの改善

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { App } from "@slack/bolt";
 import dotenv from "dotenv";
+import sendExpenses from "./send-expenses";
 
 dotenv.config();
 
@@ -27,32 +28,122 @@ app.command("/kincone", async ({ command, ack, client }) => {
           text: "Kincone Form",
         },
         blocks: [
-          {
-            type: "input",
-            block_id: "reason_block",
-            label: {
-              type: "plain_text",
-              text: "Reason",
-            },
-            element: {
-              type: "plain_text_input",
-              action_id: "reason",
-            },
-          },
+          // 利用日
           {
             type: "input",
             block_id: "date_block",
             label: {
               type: "plain_text",
-              text: "Date",
+              text: "利用日",
             },
             element: {
               type: "datepicker",
-              action_id: "date_picker",
-              initial_date: new Date().toISOString().split("T")[0],
+              action_id: "use-date",
+              initial_date: new Date().toISOString().split("T")[0], // 今日の日付を設定
+            },
+          },
+          // 交通手段
+          {
+            type: "input",
+            block_id: "translation",
+            label: {
+              type: "plain_text",
+              text: "交通手段",
+            },
+            element: {
+              type: "static_select",
+              action_id: "transportation_select",
               placeholder: {
                 type: "plain_text",
-                text: "Select a date",
+                text: "交通手段を選択",
+              },
+              options: [
+                { text: { type: "plain_text", text: "電車" }, value: "1" },
+                {
+                  text: { type: "plain_text", text: "バス/路面電車" },
+                  value: "2",
+                },
+                { text: { type: "plain_text", text: "タクシー" }, value: "3" },
+                {
+                  text: { type: "plain_text", text: "新幹線/特急" },
+                  value: "4",
+                },
+                { text: { type: "plain_text", text: "飛行機" }, value: "5" },
+                { text: { type: "plain_text", text: "車" }, value: "6" },
+                {
+                  text: { type: "plain_text", text: "船/フェリー" },
+                  value: "7",
+                },
+                { text: { type: "plain_text", text: "その他" }, value: "8" },
+                { text: { type: "plain_text", text: "物販" }, value: "0" },
+              ],
+            },
+          },
+          // 入場駅
+          {
+            type: "input",
+            block_id: "in_stateion",
+            label: {
+              type: "plain_text",
+              text: "入場駅",
+            },
+            element: {
+              type: "plain_text_input",
+              action_id: "in_staion",
+              placeholder: {
+                type: "plain_text",
+                text: "入場駅",
+              },
+            },
+          },
+          // 出場駅
+          {
+            type: "input",
+            block_id: "out_station",
+            label: {
+              type: "plain_text",
+              text: "出場駅",
+            },
+            element: {
+              type: "plain_text_input",
+              action_id: "out_station",
+              placeholder: {
+                type: "plain_text",
+                text: "出場駅",
+              },
+            },
+          },
+          // 訪問先/備考
+          {
+            type: "input",
+            block_id: "remarks",
+            label: {
+              type: "plain_text",
+              text: "訪問先/備考",
+            },
+            element: {
+              type: "plain_text_input",
+              action_id: "remarks",
+              placeholder: {
+                type: "plain_text",
+                text: "訪問先/備考",
+              },
+            },
+          },
+          // 交通費
+          {
+            type: "input",
+            block_id: "expenses",
+            label: {
+              type: "plain_text",
+              text: "交通費",
+            },
+            element: {
+              type: "plain_text_input",
+              action_id: "expenses",
+              placeholder: {
+                type: "plain_text",
+                text: "交通費",
               },
             },
           },
@@ -75,27 +166,11 @@ app.view("kincone_form", async ({ ack, body, view, client }) => {
   const reason = view.state.values.reason_block.reason.value;
   const date = view.state.values.date_block.date_picker.selected_date;
 
-  const channelId = view.private_metadata;
-  if (!channelId) {
-    console.log("undefined");
-  } else {
-    console.log(channelId);
-  }
-
   const userInfo = await client.users.profile.get({ user: body.user.id });
-  const email = userInfo.profile?.email || "Email not found";
+  const email = userInfo.profile?.email;
 
-  if (email === "Email not found") {
-  }
+  // sendExpenses();
 
-  try {
-    await client.chat.postMessage({
-      channel: channelId,
-      text: `<@${body.user.id}> has completed the Kincone form! ✅`,
-    });
-  } catch (error) {
-    console.error(error);
-  }
   console.log("Kincone Form Submitted:", { reason, date, email });
 
   // sasami botに内容を返信
